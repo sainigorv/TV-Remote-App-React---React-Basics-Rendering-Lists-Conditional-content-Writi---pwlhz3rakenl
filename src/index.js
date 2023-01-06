960bb4e7ef7b2c54fd51bba84509a0a775b35b27
sainigorv <sainigorv341@gmail.com>
2023-01-07 02:45:29 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-
-
+function myFunction(value) {
+  console.log(value);
+  document.getElementById("container").innerHTML = value;
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
